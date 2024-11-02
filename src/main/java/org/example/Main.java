bb9eaf1b2947a9ea8dde94188d3d6996db2cb489
balint.horvath <balint.horvath@instructure.com>
2024-11-02 19:07:09 +0100

    Remove element.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -290,4 +290,18 @@ public class Main {
         }
         return count;
     }
+
+    // Remove element
+    public static int removeElement(int[] nums, int val) {
+        int k = 0; // Pointer for the next insertion position
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
 }
